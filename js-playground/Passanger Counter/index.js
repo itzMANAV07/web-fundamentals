26de20af7d2e1e0524c21234cd9a23ded9b56259
Manav <myself.manav07@gmail.com>
2026-08-12 23:43:57 +0530

    Refactor index.js for passenger counter

--- js-playground/Passanger Counter/index.js
+++ js-playground/Passanger Counter/index.js
@@ -2,12 +2,10 @@ let Count=0
 let countEl = document.getElementById("count")
 console.log(countEl)
 let saveEl = document.getElementById("save-el")
-
 function increment(){
     Count += 1
     countEl.textContent=Count
 }
-
 function save(){
     let countStr = Count + " - "
     saveEl.textContent += countStr
